2bf779ea1083599888293cd704f278e8b7b5dd1f
suriyakumar132001 <suryakumar132001@gmail.com>
2026-07-21 12:08:16 +0530

    Update API URL for production

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,10 +1,16 @@
-import axios from 'axios';
+import axios from "axios";
 
-const api = axios.create({ baseURL: 'http://localhost:5000/api' });
+const api = axios.create({
+  baseURL: "https://crm-backend-gxwp.onrender.com/api",
+});
 
 api.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token');
-  if (token) config.headers.Authorization = `Bearer ${token}`;
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
   return config;
 });
 
