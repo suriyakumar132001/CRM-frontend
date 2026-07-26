7ba9de09cd1b319c3e80267beb2b1446f8c71937
suriyakumar132001 <suryakumar132001@gmail.com>
2026-07-26 15:00:02 +0530

    Fix missing /api prefix in axios baseURL

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://crm-backend-1-mxe9.onrender.com",
-
-  
+  baseURL: "https://crm-backend-1-mxe9.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
